f5f06dcfc92d8742073003102b8efbf940de22b3
eric munene <erickims08@gmail.com>
2019-07-02 17:17:28 +0300

    making pollcards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import Checkbox from "./Checkbox";
+import PropTypes from "prop-types";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const checkboxes = [
+  {
+    name: "box-1",
+    key: "checkBox1",
+    label: "Check Box 1"
+  },
+  {
+    name: "box-2",
+    key: "checkBox2",
+    label: "Check Box 2"
+  },
+  {
+    name: "box-3",
+    key: "checkBox3",
+    label: "Check Box 3"
+  },
+  {
+    name: "box-4",
+    key: "checkBox4",
+    label: "Check Box 4"
+  }
+];
+
+// const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => (
+//   <input type={type} name={name} checked={checked} onChange={onChange} />
+// );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checkedItems: new Map()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState(prevState => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked)
+    }));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="poll_card">
+          <form className="form-container">
+            <div className="poll_container">
+              {checkboxes.map(item => (
+                <label key={item.key}>
+                  <ul className="checkbox_list">
+                    {/* <Checkbox
+                name={item.name}
+                checked={this.state.checkedItems.get(item.name)}
+                onChange={this.handleChange}
+              /> */}
+                    <input
+                      type="checkbox"
+                      name={item.name}
+                      checked={this.state.checkedItems.get(item.name)}
+                      onChange={this.handleChange}
+                      className="checkbox-round"
+                    />
+                    {item.name}
+                  </ul>
+                </label>
+              ))}
+            </div>
+            <button className="confirm_button">Post Now</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
+
+App.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func.isRequired
+};
 
 export default App;
